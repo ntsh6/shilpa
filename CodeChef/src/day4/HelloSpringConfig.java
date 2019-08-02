fe543c8c444288580477e143f4340dda91a594d9
ntsh6 <nithishreddy243@gmail.com>
2019-08-02 12:54:29 +0530

    hello i m doing a branch1

---
+++ CodeChef/src/day4/HelloSpringConfig.java
@@ -0,0 +1,11 @@
+package day4;
+
+public class HelloSpringConfig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.println(" hello world ");
+
+	}
+
+}
