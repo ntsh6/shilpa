09479732901a2aabee0cd48b1619d03e5568a25f
ntsh6 <nithishreddy243@gmail.com>
2019-08-02 13:12:43 +0530

    hello commiting jwt

---
+++ CodeChef/src/day6/SpringJWT.java
@@ -0,0 +1,8 @@
+package day6;
+
+public class SpringJWT { 
+	public static void main(String[] args) {
+		System.out.println(" hello jwt");
+	}
+
+}
---
+++ CodeChef/src/day6/SpringOAUTH.java
@@ -0,0 +1,8 @@
+package day6;
+
+public class SpringOAUTH { 
+	public static void main(String[] args) {
+		System.out.println(" GOODBYE");
+	}
+
+}
