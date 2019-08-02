1b382ee99e5ec4f7ac1203bec8daf98561065ee9
ntsh6 <nithishreddy243@gmail.com>
2019-08-02 13:37:47 +0530

    hello

--- CodeChef/src/day6/SpringJWT.java
+++ CodeChef/src/day6/SpringJWT.java
@@ -1,7 +1,8 @@
 package day6;
 
 public class SpringJWT { 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+		 int b =0;
 		System.out.println(" hello Jwt");
 	}
 
